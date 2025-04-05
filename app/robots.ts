bd07bd079ec67d00b3bd429a53cf2974baccff59
wemadeathing <salaam.nasif@gmail.com>
2025-04-05 19:29:33 +0200

    Update sitemap and robots configuration to reflect new base URL for Nexevo. Changed sitemap URL and updated base URL in sitemap generation function.

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/dashboard/',
     },
-    sitemap: 'https://starter.rasmic.xyz/sitemap.xml',
+    sitemap: 'https://nexevo.io/sitemap.xml',
   }
 }
